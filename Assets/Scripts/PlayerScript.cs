4a18c3065fdadc88ebe7d3fd77c2c08a24c436bd
dgarcia202 <wft2000@gmail.com>
2017-07-09 18:49:37 +0200

    basic walk animation and facing

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -9,25 +9,48 @@ public class PlayerScript : MonoBehaviour {
 
 	private Animator animator;
 
-	void Start () {
+	private float previousAxis = 0;
+
+	public void Start () {
 		this.rigidBody = GetComponent<Rigidbody2D> ();
 		this.animator = GetComponent<Animator> ();
 	}
 
-	void Update () {
-		
+	public void Update () {
+		var horizontalInput = Input.GetAxisRaw ("Horizontal");
+		if (horizontalInput != 0f) {
+			// this.animator.SetInteger ("animState", 1);
+			this.animator.Play("PlayerWalk");
+		} else {
+			// this.animator.SetInteger ("animState", 0);
+			this.animator.Play("PlayerStill");
+		}		
 	}
 
-	void FixedUpdate () {
+	public void FixedUpdate () {
 		var horizontalInput = Input.GetAxisRaw ("Horizontal");
-		if (horizontalInput == 0f) {
-			this.animator.SetInteger ("AnimationIndex", 1);
-		} else {
-			this.animator.SetInteger ("AnimationIndex", 0);
-		}
+		ManageFacing (horizontalInput);
+		ManageSpeed (horizontalInput);
+	}
 
+	private void ManageSpeed (float horizontalInput)
+	{
 		var velocity = this.rigidBody.velocity;
 		velocity.x = horizontalInput * speed;
 		this.rigidBody.velocity = velocity;
+	}
+
+	private void ManageFacing (float horizontalInput)
+	{
+		var localScale = this.transform.localScale;
+		if (previousAxis <= 0 && horizontalInput > 0) {
+			localScale.x = 1;
+		}
+		else
+			if (previousAxis >= 0 && horizontalInput < 0) {
+				localScale.x = -1;
+			}
+		this.previousAxis = horizontalInput;
+		this.transform.localScale = localScale;
 	}
 }
