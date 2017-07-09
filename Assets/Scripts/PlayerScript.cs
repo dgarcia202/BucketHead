fff3dbface18144ccf2cd7b34d87d97e1dcb52f3
dgarcia202 <wft2000@gmail.com>
2017-07-09 16:51:25 +0200

    Initial commit

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	public float speed = 2f;
+
+	private Rigidbody2D rigidBody;
+
+	private Animator animator;
+
+	void Start () {
+		this.rigidBody = GetComponent<Rigidbody2D> ();
+		this.animator = GetComponent<Animator> ();
+	}
+
+	void Update () {
+		
+	}
+
+	void FixedUpdate () {
+		var horizontalInput = Input.GetAxisRaw ("Horizontal");
+		if (horizontalInput == 0f) {
+			this.animator.SetInteger ("AnimationIndex", 1);
+		} else {
+			this.animator.SetInteger ("AnimationIndex", 0);
+		}
+
+		var velocity = this.rigidBody.velocity;
+		velocity.x = horizontalInput * speed;
+		this.rigidBody.velocity = velocity;
+	}
+}
